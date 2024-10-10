393789d0bb50cf6202e7f12579033171e8a7a197
kakarotmuiinfinity <166479138+kakarotmuiinfinity@users.noreply.github.com>
2024-10-10 15:04:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,4 @@ def index():
     return "Bot is running!"
 
 if __name__ == "__main__":
-    import os
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
-  
+    app.run(host='0.0.0.0', port=8080)  # Set port to 8080
