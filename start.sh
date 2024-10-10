1e2311740b4a4f9cc6504d6eb1521cc7a35ba7e9
kakarotmuiinfinity <166479138+kakarotmuiinfinity@users.noreply.github.com>
2024-10-10 15:05:51 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-python3 main.py
-python main.py
-python app.py  # Start the Flask app in the background
+python app.py &  # Start the Flask app in the background
+python main.py    # Run the Telegram bot
 
