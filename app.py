702ca50b460cbb3ce363402026638b1115da3486
kakarotmuiinfinity <166479138+kakarotmuiinfinity@users.noreply.github.com>
2024-10-10 14:54:22 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "Bot is running!"
+
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+  
